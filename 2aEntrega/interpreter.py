d9b012bd60ec86524c9d0b7c43377cdee8bed00e
GabSeven - arch <gabrodrigomes@gmail.com>
2026-06-30 18:03:45 -0300

    testando cores no interpreter

--- 2aEntrega/interpreter.py
+++ 2aEntrega/interpreter.py
@@ -1,10 +1,33 @@
+from sys import argv
 
+import rich
 
-def main(instructions: list[]) -> None:
+undefined = object()
+
+def log(obj):
+    if obj == undefined:
+        rich.print(r"[yellow]undefined")
+    else:
+        rich.print(r"[blue]" + obj)
+def main(instructions: list[list[str]]) -> None:
+    stack = dict()
     
-    return
+    for instruction in instructions:
+        match instruction[0]:
+            case "def":
+                stack[instruction[1]] = undefined
+            case "set":
+                stack[instruction[1]] = instruction[2]
+            case "log":
+                log(stack[instruction[1]])
+                
 
 if __name__ == "__main__":
-    
+    instructions: list[list[str]] = []
+    with open(argv[1], "r") as bytecode:
+        for line in bytecode:
+            if line.strip():
+                instructions.append(line.split())
+        # print(instructions)
 
-    main()
+    main(instructions)
